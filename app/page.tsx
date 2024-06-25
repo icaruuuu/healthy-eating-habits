4499d5ff2ae409d6363c4e2ee08d4522472e8db0
icaruuuu <ericamae.cape@gmail.com>
2024-06-25 22:32:19 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
             Take the Survey
           </a>
         </Link>
+        <Link href="/analytics/AnalyticsPage">View Analytics</Link>
       </div>
     </main>
   );
