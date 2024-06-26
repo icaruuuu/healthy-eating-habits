a28a535151afd14aa99676b8ea2c3ad649047c04
icaruuuu <ericamae.cape@gmail.com>
2024-06-26 10:06:48 +0800

    1

--- models/HealthSurvey.js
+++ models/HealthSurvey.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const healthSurveySchema = new mongoose.Schema({
+const responsesSchema = new mongoose.Schema({
     name: String,
     age: Number,
     gender: String,
@@ -17,4 +17,4 @@ const healthSurveySchema = new mongoose.Schema({
     class_attendance: Number,
 });
 
-export default mongoose.models.HealthSurvey || mongoose.model('HealthSurvey', healthSurveySchema);
+export default mongoose.models.HealthSurvey || mongoose.model('Responses', responsesSchema );
