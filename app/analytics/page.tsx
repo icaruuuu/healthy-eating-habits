4d8e79af0cf55e39f85ad98f084aae8b56778a0e
icaruuuu <ericamae.cape@gmail.com>
2024-06-26 16:50:32 +0800

    Update page.tsx

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -100,10 +100,25 @@ const GraphPage: React.FC = () => {
   const fruitVegetableChartData = {
     labels: Array.from(Array(fruitVegetableData.length).keys()).map(String),
     datasets: [{
-      label: 'Fruits and Vegetables Consumption',
+      label: ['0', '1', '2', '3', '4', '5'],
       data: fruitVegetableData,
       fill: false,
-      borderColor: 'rgba(75, 192, 192, 1)',
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 102, 255, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 102, 255, 1)',
+      ],
       tension: 0.1,
     }]
   };
