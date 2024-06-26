6de4f359c811015e39caf35bf79964479f5fd506
icaruuuu <ericamae.cape@gmail.com>
2024-06-26 16:29:58 +0800

    Update page.tsx

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -39,7 +39,7 @@ const GraphPage: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('/api/get-health-survey-data'); // Adjust API endpoint
+        const response = await axios.get('/api/get-survey');
         console.log("Health survey data received:", response.data);
         setSurveyData(response.data);
       } catch (error) {
